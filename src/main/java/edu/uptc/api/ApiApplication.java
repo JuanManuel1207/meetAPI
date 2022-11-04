6aa96e182080b489f31d78a516794e6636fda344
JuanManuel1207 <juan.lopez14@uptc.edu.co>
2022-11-04 16:43:39 -0500

    prueba

--- src/main/java/edu/uptc/api/ApiApplication.java
+++ src/main/java/edu/uptc/api/ApiApplication.java
@@ -20,7 +20,7 @@ public class ApiApplication {
 
 
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*");
+			public void addCorsMappings(CorsRegistry registry) {registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
